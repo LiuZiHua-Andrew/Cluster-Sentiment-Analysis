235016f100fe35b61ccfe854b1b08ad36ee31531
lingtaoj <lingtaoj@student.unimelb.edu.au>
2018-05-03 19:40:09 +1000

    Update TwitterHarvest

--- Sentiment Analysis/SentimentAnalysis.py
+++ Sentiment Analysis/SentimentAnalysis.py
@@ -2,65 +2,114 @@ from textblob import TextBlob
 import couchdb
 import json
 from geojson_utils import point_in_polygon
+from nltk.tokenize import TweetTokenizer
+import sys
+import getopt
+def preprocess(text):
+    tokenizer = TweetTokenizer()
+    tokens = tokenizer.tokenize(text)
+
+    for token in tokens[:]:
+        if token[0] == u'@' or token[0] == u'#'  or (len(token) >= 7 and token.lower()[:7] == u'http://') or (len(token) >= 8 and token.lower()[:8] == u'https://'):
+            tokens.remove(token)
+
+    sent =''
+    for token in tokens:
+        sent += token + ' '
+    return sent.strip()
 
 
-def sentiment_polarity(db,suburbs):
-    suburb_tweets_count = dict()
-    for suburb in suburbs:
-        suburb_tweets_count[suburb['properties']['Suburb_Name']] = 0
-    polarities = []
+
+
+def sentiment_polarity(db_json,suburbs):
+    with open(db_json) as f:
+        db_info = f.read()
+    db_info = json.loads(db_info)
+
+    couch_host = db_info['host']
+    couch_port = db_info['port']
+    db_name = db_info['database']
+    db_name2 = db_info['database2']
+
+    host_and_port = "http://" + couch_host + ":" + str(couch_port)
+    couch = couchdb.Server(host_and_port)
+
+    try:
+        db = couch[db_name]
+    except:
+        exit(-1)
+
+    try:
+        couch.delete(db_name2)
+    except:
+        pass
+
+    db2 = couch.create(db_name2)
+
+    processed_data = []
     for id in db:
         twitter = db[id]
-        text = twitter['text']
-        blob = TextBlob(text)
-        if not twitter['coordinates'] == None:
-            for suburb in suburbs:
-                if point_in_polygon(twitter['coordinates'], suburb['geometry']):
-                    suburb_tweets_count[suburb['properties']['Suburb_Name']] += 1
-                    break
+        try:
+            text = twitter['extended_tweet']['full_text']
+        except:
+            text = twitter['text']
 
+        blob = TextBlob(text)
         if not twitter['lang'] == 'en':  # If the language is not English, translate it.
             try:
                 blob = blob.translate(from_lang=twitter['lang'], to="en")
             except:
-                pass
+                continue
 
-        # blob = blob.correct()
-        # print(blob.tags)
+        blob = TextBlob(preprocess(blob.raw))
 
-        for sentence in blob.sentences:
-            polarities.append((sentence, sentence.sentiment.polarity))
-    a = sorted(suburb_tweets_count.items(), key=lambda x: x[1], reverse=True)
+        s = None
+        if not twitter['coordinates'] == None:
+            for suburb in suburbs:
+                if point_in_polygon(twitter['coordinates'], suburb['geometry']):
+                    # suburb_tweets_count[suburb['properties']['Suburb_Name']] += 1
+                    s = suburb['properties']['Suburb_Name']
+                    break
 
-    return polarities
+            if s != None:
+                analysis_data = dict()
+                analysis_data['_id'] = twitter['id_str']
+                analysis_data['suburb'] = s
+                analysis_data['hashtags'] = [ht['text'] for ht in twitter['entities']['hashtags']]
+                analysis_data['polarity'] = blob.sentiment.polarity
+                processed_data.append(analysis_data)
+                if len(processed_data) == 10:
+                    db2.update(processed_data)
+                    processed_data = []
+
+    if len(processed_data) > 0:
+        db2.update(processed_data)
 
 
-def hashtag_ranking(db):
-    hts = dict()
-    for id in db:
-        twitter = db[id]
-        ht = twitter['entities']['hashtags']
-        for h in ht:
-            hts[h['text']] = hts.get(h['text'], 0) + 1
-    return (sorted(hts.items(), key=lambda x: x[1], reverse=True))
+def main(argv):
+    db_json = 'db.json'
+    try:
+        opts, args = getopt.getopt(argv, "hd:", ['database='])
+    except getopt.GetoptError:
+        print ("""-d <database_json>""")
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print ("""-d <database_json>""")
+            sys.exit()
+        elif opt in ("-d", "--database"):
+            db_json = arg
 
 
-with open("vic.json") as f:
-    grids_str =f.read()
+    with open("vic.json") as f:
+        grids_str = f.read()
+    suburbs = json.loads(grids_str)
 
-suburbs = json.loads(grids_str)
+    sentiment_polarity(db_json,suburbs)
 
-couch_host = "localhost"
-couch_port = 5984
-db_name = "test_au"
 
-host_and_port = "http://"+couch_host+":"+str(couch_port)
-couch = couchdb.Server(host_and_port)
+if __name__ == "__main__":
+   main(sys.argv[1:])
 
-try:
-    db = couch[db_name]
-except:
-    exit(-1)
 
-sentiment_polarity(db,suburbs)
 
