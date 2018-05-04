607c4bd3a9b78a11b43ae4b29b96ac5e4e487a8b
Zhenxiang Wang <30854448+ZhenxiangWang@users.noreply.github.com>
2018-05-04 16:46:17 +1000

    Add files via upload

--- TwitterGet/getTwitter.py
+++ TwitterGet/getTwitter.py
@@ -1,4 +1,5 @@
 import requests
+import couchdb
 auth=('readonly','ween7ighai9gahR6')
 
 def get_geocoded_tweets(include_docs='true',reduce='false',skip='0',limit=None,auth=('readonly','ween7ighai9gahR6')):
@@ -25,8 +26,8 @@ def get_geo_json(reduce='false',start_key='\[\"r1r0\",2014,1,1\]',end_key='\[\"r
 
     auth=auth
     r=requests.get(url=url,params=payload,auth=auth)
-
-    return r.json()
+    return r
+    # return r.json()
 
 def get_aggregated_tweets_by_city(include_docs='false',reduce='true',group_level='1',skip='0',limit=None,auth=('readonly','ween7ighai9gahR6')):
     url="http://45.113.232.90/couchdbro/twitter/_design/twitter/_view/summary"
@@ -40,11 +41,34 @@ def get_aggregated_tweets_by_city(include_docs='false',reduce='true',group_level
 
     return r.json()
 
-geocoded_tweets=get_geocoded_tweets(limit='2',auth=auth)
-print(geocoded_tweets)
+# geocoded_tweets=get_geocoded_tweets(skip='1',limit='1',auth=auth)
+# print(geocoded_tweets)
 
 geo_json=get_geo_json(limit='2',auth=auth)
 print(geo_json)
+#
+# aggregated_tweets_by_city=get_aggregated_tweets_by_city(limit='20',auth=auth)
+# print(aggregated_tweets_by_city)
 
-aggregated_tweets_by_city=get_aggregated_tweets_by_city(limit='2',auth=auth)
-print(aggregated_tweets_by_city)
+#########################################################################################################################################
+def put_data_into_couchdb():
+    couch_host = "localhost"
+    couch_port = 5984
+    db_name = "geocoded_tweets"
+    total_rows=3629417
+
+    host_and_port = "http://" + couch_host + ":" + str(couch_port)
+    couch = couchdb.Server(host_and_port)
+    try:
+        db = couch.create(db_name)  # create db
+    except:
+        db = couch[db_name]  # existing
+
+    for i in range(total_rows):
+        skip=str(i)
+        geocoded_tweets = get_geocoded_tweets(skip=skip, limit='1', auth=auth)
+
+
+        db.save(geocoded_tweets)
+
+# put_data_into_couchdb()
