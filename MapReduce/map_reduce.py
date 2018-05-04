103b98bb9030cdba89ae810beab10dcbaa313e1b
Zhenxiang Wang <30854448+ZhenxiangWang@users.noreply.github.com>
2018-05-04 21:33:12 +1000

    Add files via upload

--- MapReduce/map_reduce.py
+++ MapReduce/map_reduce.py
@@ -1,23 +1,27 @@
 import couchdb,sys
 from couchdb.design import ViewDefinition
 
-couch=couchdb.Server('http://localhost:5984/')
-
-print(couch)
-
-db=couch['geocoded_tweets']
-
-print(db)
-
+couchServer=couchdb.Server('http://localhost:5984/')
+database='geocoded_tweets'
 designDocument='newDesignDocument'
 viewName='polarity'
 map_fun='''function(doc) { emit(doc.suburb, doc.polarity);};}'''
 reduce_fun='''function(keys, values) { return sum(values)/values.length }'''
 
-view=ViewDefinition(design=designDocument,name=viewName,
-                    map_fun=map_fun,
-                    reduce_fun=reduce_fun)
+def map_reduce(couchServer,database,designDocument,viewName,map_fun,reduce_fun):
+    couch=couchServer
+    print(couch)
 
-view.sync(db)
+    db=couch[database]
+    print(db)
 
-print('complete!')
+    designDocument=designDocument
+    viewName=viewName
+    map_fun=map_fun
+    reduce_fun=reduce_fun
+    view=ViewDefinition(design=designDocument,name=viewName,
+                        map_fun=map_fun,
+                        reduce_fun=reduce_fun)
+    view.sync(db)
+    print('complete!')
+map_reduce(couchServer,database,designDocument,viewName,map_fun,reduce_fun)
